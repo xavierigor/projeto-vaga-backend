8e8dd32cd0ba6ea71920d833f170a0cbb5200878
Igor Xavier <igoorlimaa@gmail.com>
2022-04-22 23:04:00 -0300

    Added TestingConfiguration + tests

--- app/main/config.py
+++ app/main/config.py
@@ -12,10 +12,21 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    ERROR_404_HELP = False
+
+
+class TestingConfig(Config):
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(
+        basedir, 'flask_boilerplate_test.db')
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 config_by_name = dict(
-    dev=DevelopmentConfig
+    dev=DevelopmentConfig,
+    test=TestingConfig,
 )
 
 key = Config.SECRET_KEY
