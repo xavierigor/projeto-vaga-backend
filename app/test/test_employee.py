bc9e9125954cb7b36164d9d9835b8144124fc84a
Igor Xavier <igoorlimaa@gmail.com>
2022-04-24 16:30:33 -0300

    New test case: /employees/(id)/dependents sub-endpoint

--- app/test/test_employee.py
+++ app/test/test_employee.py
@@ -1,6 +1,7 @@
 import unittest
 
 from app.main.model.department import Department
+from app.main.model.dependent import Dependent
 from app.main.model.employee import Employee
 from app.test.base import BaseTestCase
 from app.test.util import create_instance
@@ -42,6 +43,25 @@ class TestEmployee(BaseTestCase):
             data = response.json
             self.assertEquals(data['full_name'], 'John Doe')
             self.assertEquals(data['department_id'], 1)
+            self.assertEquals(response.status_code, 200)
+
+    def test_get_employees_dependents(self):
+        create_instance(Employee, full_name='Clifford Price', department_id=2)
+        create_instance(Dependent, full_name='Cyrus Wall', employee_id=1)
+        create_instance(Dependent, full_name='Cassia Callaghan', employee_id=2)
+        with self.client:
+            response = self.client.get('/employees/1/dependents')
+            data = response.json
+            self.assertIsInstance(data, list)
+            self.assertEquals(len(data), 1)
+            self.assertEquals(response.status_code, 200)
+            dependent = data[0]
+            self.assertEquals(dependent['full_name'], 'Cyrus Wall')
+            self.assertEquals(dependent['employee_id'], 1)
+            response = self.client.get('/employees/3/dependents')
+            data = response.json
+            self.assertIsInstance(data, list)
+            self.assertEquals(len(data), 0)
             self.assertEquals(response.status_code, 200)
 
 
